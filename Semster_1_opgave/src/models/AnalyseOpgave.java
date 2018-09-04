b0fb0a490e27757492817484aa6f8838c965abc2
Michael <Inztagib@hotmail.com>
2018-09-04 11:39:38 +0200

    First time upload

---
+++ Semster_1_opgave/src/models/AnalyseOpgave.java
@@ -0,0 +1,19 @@
+package models;
+
+public class AnalyseOpgave extends PraktikOpgave {
+
+    private String analyseModel;
+
+    public AnalyseOpgave(String navn, int semester, PraktikVirksomhed virksomhed, String analyseModel) {
+        super(navn, semester);
+        this.analyseModel = analyseModel;
+
+    }
+
+    @Override
+    public String toString() {
+        return super.getNavn() + " " + super.getSemester() + " " + this.analyseModel;
+
+    }
+
+}
---
+++ Semster_1_opgave/src/models/PraktikOpgave.java
@@ -0,0 +1,25 @@
+package models;
+
+public class PraktikOpgave {
+    private String navn;
+    private int semester;
+    private PraktikVirksomhed virksomhed;
+
+    public PraktikOpgave(String navn, int semester) {
+        this.navn = navn;
+        this.semester = semester;
+
+    }
+
+    public String getNavn() {
+        return navn;
+    }
+
+    public int getSemester() {
+        return semester;
+    }
+
+    public PraktikVirksomhed getVirksomhed() {
+        return virksomhed;
+    }
+}
