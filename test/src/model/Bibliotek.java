5192c57588a7a25d8756636ea24257563bbe2992
Michael <Inztagib@hotmail.com>
2018-09-10 11:06:31 +0200

    Photon_Fuck

---
+++ test/src/model/Bibliotek.java
@@ -0,0 +1,22 @@
+package model;
+
+import java.time.LocalDate;
+
+public class Bibliotek {
+    public Bibliotek() {
+    }
+
+    /*
+     * * Returnerer størrelsen af bøden beregnet i henhold til skemaet * ovenfor
+     * * krav: beregnetDato < faktiskDato * (beregnetDato er forventet
+     * afleveringsdato og * faktiskDato er den dag bogen blev afleveret; voksen
+     * er * sand, hvis det er en voksen og falsk ellers)
+     */
+    public int beregnBøde(LocalDate beregnetDato, LocalDate faktiskDato, boolean voksen) {
+
+        if (beregnetDato.isAfter(faktiskDato)) {
+            throw new RuntimeException();
+        }
+        return 0;
+    }
+}
