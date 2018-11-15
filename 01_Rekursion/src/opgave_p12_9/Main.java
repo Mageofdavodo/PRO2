6844a5dc645a3d85807ee010bb4bff71e8e336ad
Michael <Inztagib@hotmail.com>
2018-11-15 08:37:52 +0100

    might fix

--- 01_Rekursion/src/opgave_p12_9/Main.java
+++ 01_Rekursion/src/opgave_p12_9/Main.java
@@ -19,7 +19,6 @@ public class Main {
     public static void main(String[] args) {
         String s = "Fisk";
         System.out.println(reverseString(s));
-        System.out.println();
 
     }
 
